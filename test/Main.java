428d192d5f02a104becc55e493590a0b33024c22
Gabriel Garcia <gabriel.garcia@estudiants.urv.cat>
2021-12-08 15:06:10 +0100

    Funciones de lectura de ficheros de prueba realizadas

--- test/Main.java
+++ test/Main.java
@@ -1,5 +1,5 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -15,28 +15,77 @@ public class Main {
         }
         System.out.println("Nombre del fichero?");
         filename = scanner.next();
-        scanner.close();
         if(modo==1) {
-            ListConsultaEstatica listaEstatica = leerFicheroEstatico(filename);
+            System.out.println("Numero de consultas?");
+            int dim = scanner.nextInt();
+            ListConsultaEstatica listaEstatica = null;
+            try {
+                listaEstatica = leerFicheroEstatico(filename,dim);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             testEstatico(listaEstatica);
             ListRecursosEstatico listaREstatica = consultaToRecursoEstatic(listaEstatica);
             testREstatico(listaREstatica);
         }
         else {
-            ListConsultaDinamica listaDinamica = leerFicheroDinamico(filename);
+            ListConsultaDinamica listaDinamica = null;
+            try {
+                listaDinamica = leerFicheroDinamico(filename);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             testDinamico(listaDinamica);
             ListRecursosDinamico listaRDinamica = consultaToRecursoDinamic(listaDinamica);
             testRDinamico(listaRDinamica);
         }
+        scanner.close();
     }
 
-    public static ListConsultaEstatica leerFicheroEstatico(String filename) {
-
+    public static ListConsultaEstatica leerFicheroEstatico(String filename,int dim) throws IOException {
+        Scanner sc;
+        sc = new Scanner(new File(filename));
+        ListConsultaEstatica listaEstatica = new ListConsultaEstatica(dim);
+        // Check if there is another line of input
+        while(sc.hasNextLine()) {
+            String str = sc.nextLine();
+                Consulta c = parseData(str);
+                listaEstatica.addConsulta(c);
+        }
+        return listaEstatica;
     }
 
-    public static ListConsultaDinamica leerFicheroDinamico(String filename) {
-
+    public static ListConsultaDinamica leerFicheroDinamico(String filename) throws IOException{
+        Scanner sc;
+        sc = new Scanner(new File(filename));
+        ListConsultaDinamica listaDinamica = new ListConsultaDinamica();
+        // Check if there is another line of input
+        while(sc.hasNextLine()) {
+            String str = sc.nextLine();
+            // parse each line using delimiter
+            Consulta c = parseData(str);
+            listaDinamica.addConsulta(c);
+        }
+        return listaDinamica;
     }
+
+    private static Consulta parseData(String str) {
+        int data, hora;
+        String recurs, user;
+        Scanner lineScanner = new Scanner(str);
+        Consulta c = null;
+        lineScanner.useDelimiter(";");
+        while(lineScanner.hasNext()){
+            user = lineScanner.next();
+            data = lineScanner.nextInt();
+            hora = lineScanner.nextInt();
+            recurs = lineScanner.next();
+            c = new Consulta(user,recurs,data,hora);
+        }
+        lineScanner.close();
+        return c;
+    }
+
 
     public static void testEstatico(ListConsultaEstatica listaEstatica) {
 
